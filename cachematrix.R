8a68dea9f9bc6c1cf3d2a62eb9e36db37951ccc5
Pierre Poulain <pierre.poulain@cupnet.net>
2014-08-18 21:57:46 +0100

    Set up prototypes for makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a list object with the cached inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## "x" is a matrix object
+    
+    ## the return list has 4 elements:
+    ## set(): define/create the matrix
+    ## get(): get the content of the matrix
+    ## setinverse(): define the inverse of the matrix
+    ## getinverse(): retrieve the cached inverse of the matrix
+    
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve computes inverse of an object produces by makeCacheMatrix()
+## at first run, the inverse matrix is computed and cached
+## other time, the inverse matrix is retrieved from cache memory
 cacheSolve <- function(x, ...) {
+        ## "x" is a an object produced by makeCacheMatrix
         ## Return a matrix that is the inverse of 'x'
 }
