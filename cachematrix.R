1b8707aac5c457ebca979914fe18f2ee4051eaed
Pierre Poulain <pierre.poulain@cupnet.net>
2014-08-18 22:09:04 +0100

    Cleaning example functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,6 @@
 ## functions do
 
 ## makeCacheMatrix creates a list object with the cached inverse matrix
-makeVector <- function(x = numeric()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-}
 makeCacheMatrix <- function(x = matrix()) {
     ## "x" is a matrix object
     
@@ -40,17 +27,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve computes inverse of an object produces by makeCacheMatrix()
 ## at first run, the inverse matrix is computed and cached
 ## other time, the inverse matrix is retrieved from cache memory
-cachemean <- function(x, ...) {
-    m <- x$getmean()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
-    m
-}
 cacheSolve <- function(x, ...) {
     ## "x" is a an object produced by makeCacheMatrix
     ## Return a matrix that is the inverse of 'x'
