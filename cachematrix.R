dfe78a8f7956c61076af216a68c0d8b0790435bb
Pierre Poulain <pierre.poulain@cupnet.net>
2014-08-18 22:02:18 +0100

    Import makeVector and cachemean functions as examples

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,19 @@
 ## functions do
 
 ## makeCacheMatrix creates a list object with the cached inverse matrix
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
 makeCacheMatrix <- function(x = matrix()) {
     ## "x" is a matrix object
     
@@ -17,6 +30,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve computes inverse of an object produces by makeCacheMatrix()
 ## at first run, the inverse matrix is computed and cached
 ## other time, the inverse matrix is retrieved from cache memory
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
 cacheSolve <- function(x, ...) {
         ## "x" is a an object produced by makeCacheMatrix
         ## Return a matrix that is the inverse of 'x'
